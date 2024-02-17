4854434862515d438c16a1cd28dd672b14858660
GamesOfShadows <tim.lotterer@gmx.de>
2024-02-17 18:18:48 +0100

    Updated GoS_BlackBorderTouch (Version 1.1)
    Added option to activate a switch or common-event

--- GoS_BlackBorderTouch.js
+++ GoS_BlackBorderTouch.js
@@ -5,6 +5,8 @@
 * With this plugin (for RPGmaker MV & MZ) you can allow left- &
 * right-click input on the black borders.
 * 
+* You can also activate a switch or common event.
+* 
 * If you resize your game, you can get those black borders,
 * good for e.g. minigames on smartphone.
 * 
@@ -14,7 +16,7 @@
 * 
 * ------------------------------------------------------------
 * 
-* Version: 1.0
+* Version: 1.1
 * 
 * - You can do everything you want with this plugin
 * - You can use it commercially
@@ -32,6 +34,10 @@
 * @value Allow
 * @option Allow, but change to right-click
 * @value AllowButChange
+* @option Allow, but activate switch
+* @value AllowButUseSwitch
+* @option Allow, but activate common-event
+* @value AllowButUseCE
 * @option Deny
 * @value Deny
 * @default Allow
@@ -44,15 +50,33 @@
 * @value Allow
 * @option Allow, but change to left-click
 * @value AllowButChange
+* @option Allow, but activate switch
+* @value AllowButUseSwitch
+* @option Allow, but activate common-event
+* @value AllowButUseCE
 * @option Deny
 * @value Deny
 * @default Allow
+* 
+* @param IDforLeftClickOutsideCanvas
+* @text Left-Click Switch/CE (Id)
+* @desc Activate switch or common-event with given Id
+* @type number
+* @default 0
+* 
+* @param IDforRightClickOutsideCanvas
+* @text Right-Click Switch/CE (Id)
+* @desc Activate switch or common-event with given Id
+* @type number
+* @default 0
 */
 
 (() => {
 	const pluginName = "GoS_BlackBorderTouch";
 	let LeftClick = PluginManager.parameters(pluginName).allowLeftClickOutsideCanvas;
 	let RightClick = PluginManager.parameters(pluginName).allowRightClickOutsideCanvas;
+	let IDforLeftClick = PluginManager.parameters(pluginName).IDforLeftClickOutsideCanvas;
+	let IDforRightClick = PluginManager.parameters(pluginName).IDforRightClickOutsideCanvas;
 	TouchInput._onLeftButtonDown = function(event) {
 		const x = Graphics.pageToCanvasX(event.pageX);
 		const y = Graphics.pageToCanvasY(event.pageY);
@@ -62,6 +86,10 @@
 			Graphics.isInsideCanvas(x, y) ? this._onTrigger(x, y) : "";
 		} else if (LeftClick == "AllowButChange") {
 			this._onCancel(x, y);
+		} else if (LeftClick == "AllowButUseSwitch") {
+			$gameSwitches.setValue(IDforLeftClick, true);
+		} else if (LeftClick == "AllowButUseCE") {
+			$gameTemp.reserveCommonEvent(IDforLeftClick);
 		};
 	};
 
@@ -74,6 +102,10 @@
 			this._mousePressed = true;
 			this._pressedTime = 0;
 			Graphics.isInsideCanvas(x, y) ? this._onTrigger(x, y) : "";
+		} else if (RightClick == "AllowButUseSwitch") {
+			$gameSwitches.setValue(IDforLeftClick, true);
+		} else if (RightClick == "AllowButUseCE") {
+			$gameTemp.reserveCommonEvent(IDforLeftClick);
 		};
 	};
 
